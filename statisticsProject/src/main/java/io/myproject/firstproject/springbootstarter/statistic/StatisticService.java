fd2568a5f0882bdab1eb26f2b047824c9febc0f0
idosi <30891725+idosi@users.noreply.github.com>
2017-09-28 14:37:51 +0300

    Add files via upload

--- statisticsProject/src/main/java/io/myproject/firstproject/springbootstarter/statistic/StatisticService.java
+++ statisticsProject/src/main/java/io/myproject/firstproject/springbootstarter/statistic/StatisticService.java
@@ -12,20 +12,19 @@ public class StatisticService implements StatServiceInterface
 {
 	private Statistic stat;
 	private List<Integer> numberList = new ArrayList<>();
-
+	
 	@Value("${base.module.percentileArray}")
 	private String[] percentileArray;
 
 	private Double getAvg()
 	{
 		double sum = getSum();
-		if(sum > 0)
+		if (sum>0)
 		{
-			Double avg = sum / numberList.size();
+			Double avg = sum/numberList.size();
 			return avg;
 		}
-		else
-		{
+		else{
 			return sum;
 		}
 	}
@@ -33,17 +32,14 @@ public class StatisticService implements StatServiceInterface
 	private Long getMedian()
 	{
 		Long medianValue = null;
-		Long[] numbersArr = numberList.toArray(new Long[numberList.size()]);
+		Integer[] numbersArr = numberList.toArray(new Integer[numberList.size()]);
 		Arrays.sort(numbersArr);
-		int median = numbersArr.length / 2;
-		if(numbersArr.length % 2 == 1)
-		{
-			medianValue = (long) numbersArr[median];
+		int median = numbersArr.length /2;
+		if (numbersArr.length % 2 == 1) {
+			medianValue = (long)numbersArr[median];
 			return medianValue;
-		}
-		else
-		{
-			medianValue = (numbersArr[median - 1] + numbersArr[median]) / 2;
+		} else {
+			medianValue = (long)(numbersArr[median - 1] + numbersArr[median]) / 2;
 			return medianValue;
 		}
 	}
@@ -59,29 +55,26 @@ public class StatisticService implements StatServiceInterface
 	}
 
 	private int[] getPercentiles()
-	{
+	{		
 		Integer[] numbersArr = numberList.toArray(new Integer[numberList.size()]);
 		Double[] percentiles = getDoubleArray(percentileArray);
-		int[] percentilesResult = percentiles(numbersArr, percentiles);
-		return percentilesResult;
+		int[] percentilesResult = percentiles(numbersArr , percentiles);
+		return percentilesResult;	
 	}
 
 	private Double[] getDoubleArray(String[] percentileArray)
 	{
 		Double[] percentilesLongArr = new Double[percentileArray.length];
-		for(int i = 0 ; i < percentileArray.length ; i++)
-		{
+		for (int i = 0; i < percentileArray.length; i++) {
 			percentilesLongArr[i] = Double.parseDouble(percentileArray[i]);
 		}
 		return percentilesLongArr;
 	}
 
-	private static int[] percentiles(Integer[] latencies, Double... percentiles)
-	{
+	private static int[] percentiles(Integer[] latencies, Double... percentiles) {
 		Arrays.sort(latencies, 0, latencies.length);
 		int[] values = new int[percentiles.length];
-		for(int i = 0 ; i < percentiles.length ; i++)
-		{
+		for (int i = 0; i < percentiles.length; i++) {
 			int index = (int) (percentiles[i] * latencies.length);
 			values[i] = latencies[index];
 		}
@@ -90,17 +83,17 @@ public class StatisticService implements StatServiceInterface
 
 	public String getSpecificStatistic(String specificStats)
 	{
-		if(StatisticEnum.AVERAGE.toString().toLowerCase().equals(specificStats))
+		if (StatisticEnum.AVERAGE.toString().toLowerCase().equals(specificStats))
 		{
 			Double avg = getAvg();
 			return "Average = " + avg.toString();
 		}
-		if(StatisticEnum.SUM.toString().toLowerCase().equals(specificStats))
+		if (StatisticEnum.SUM.toString().toLowerCase().equals(specificStats))
 		{
 			Double sum = getSum();
-			return "Sum = " + sum.toString();
+			return  "Sum = " + sum.toString();
 		}
-		if(StatisticEnum.MEDIAN.toString().toLowerCase().equals(specificStats))
+		if (StatisticEnum.MEDIAN.toString().toLowerCase().equals(specificStats))
 		{
 			Long median = getMedian();
 			return "Median = " + median.toString();
@@ -108,10 +101,10 @@ public class StatisticService implements StatServiceInterface
 
 		return "input value is not correct";
 	}
-
+	
 	public Statistic getStatistic()
 	{
-		if(numberList.isEmpty())
+		if (numberList.isEmpty())
 		{
 			return null;
 		}
@@ -119,8 +112,7 @@ public class StatisticService implements StatServiceInterface
 		Double avg = getAvg();
 		Long median = getMedian();
 		int[] percentiles = getPercentiles();
-		stat = new StatisticBuilder(numberList).setSum(sum).setAverage(avg).setMedian(median).setPercentiles(percentiles)
-		        .setNumberCount(numberList.size()).build();
+		stat = new StatisticBuilder(numberList).setSum(sum).setAverage(avg).setMedian(median).setPercentiles(percentiles).setNumberCount(numberList.size()).build();
 		return stat;
 	}
 
